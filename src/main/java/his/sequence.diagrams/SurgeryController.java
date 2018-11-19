6182cc505834835df9766f7c8ec19cbd9a38fc1c
untar1 <01.stass@gmail.com>
2018-11-19 19:43:48 +0200

    added more controller code

--- src/main/java/his/sequence.diagrams/SurgeryController.java
+++ src/main/java/his/sequence.diagrams/SurgeryController.java
@@ -5,6 +5,7 @@
 package his.sequence.diagrams;
 
 import his.DateTime;
+import his.Folder;
 import his.Surgeon;
 import his.implementation.HasBoundary;
 
@@ -13,22 +14,48 @@ import java.util.Collection;
 public class SurgeryController implements HasBoundary
 {
     private Boundary boundary;
+    private SurgeryDAO surgeryDAO;
+    private UserDAO userDAO;
+    private FolderDAO folderDAO;
 
-	public String bookSurgery( String patientId, DateTime date, Surgeon surgeon )
+
+    public String bookSurgery(String patientId, DateTime date, Surgeon surgeon )
 	{
-		return null;
+        if (!boundary.getCurrentUserRole().equalsIgnoreCase("Oncologist")) {
+            boundary.error("Not enough permissions");
+        }
+        String surgeonId = surgeon.getId();
+        if (!userDAO.patientWithIdExists(patientId)) boundary.error("Patient with ID '" + patientId + "' does not exist");
+        if (!userDAO.surgeonWithIdExists(surgeonId)) boundary.error("Surgeon with ID '" + patientId + "' does not exist");
+        if (!surgeryDAO.surgeonAvailable(surgeonId, date)) boundary.error("Surgeon not available");
+        String surgeryId = surgeryDAO.bookSurgery(surgeonId, date);
+        Folder folder = folderDAO.getPatientFolder(patientId);
+        folderDAO.addSurgery(folder, surgeryId);
+
+		return surgeryId;
 	}
 	
 	public boolean setSurgeonTeam( String surgeonId, DateTime dateTime, Collection teamList )
 	{
-		return false;
+        if (!boundary.getCurrentUserRole().equalsIgnoreCase("Surgeon")) {
+            boundary.error("Not enough permissions");
+        }
+        if (surgeryDAO.getSurgeryId(surgeonId, dateTime) == null) boundary.error("Surgery does not exist");
+
+	    return false;
 	}
 	
 	public boolean isInAllowedPeriod( DateTime curr, DateTime surgDate )
 	{
-		return false;
+
+	    return false;
 	}
 
+    public SurgeryController(SurgeryDAO surgeryDAO, UserDAO userDAO, FolderDAO folderDAO) {
+        this.surgeryDAO = surgeryDAO;
+        this.userDAO = userDAO;
+        this.folderDAO = folderDAO;
+    }
 
     public void setBoundary(Boundary boundary) {
         this.boundary = boundary;
